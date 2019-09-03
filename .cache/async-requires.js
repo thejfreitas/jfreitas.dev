ab96b283e9aebede486a165a888ede36d2a20661
junior.freitas <juniormfreitas@gmail.com>
2019-09-02 20:00:50 -0600

    Pulling Gatsby template

---
+++ .cache/async-requires.js
@@ -0,0 +1,8 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/coconutcalendar/Documents/labs/jfreitas.dev/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/coconutcalendar/Documents/labs/jfreitas.dev/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
