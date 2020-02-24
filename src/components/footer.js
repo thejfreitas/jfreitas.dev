a80eef1da7a96372b18a5fffd7e5e36919db5c6f
junior.freitas <juniormfreitas@gmail.com>
2020-02-23 18:35:53 -0600

    Adding new images

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,11 +5,11 @@ export default () => {
 
     return (
         <footer className="container pure-g">
-            <div className="pure-u-1-2">
-                <p>jfreitas.dev {year} | Handcrafted by me</p>
+            <div className="pure-u-1 pure-u-md-1-2 credits">
+                <p>jfreitas.dev {year} &bull; Handcrafted by me</p>
             </div>
-            <div className="pure-u-1-2">
-                <p>How I built this website</p>
+            <div className="pure-u-1 pure-u-md-1-2 code-source">
+                <p><a href="https://github.com/juniormfreitas/jfreitas.dev" target="_blank" rel="noopener noreferrer">How I built this website</a></p>
             </div>
         </footer>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,6 @@ export default () => {
     <Layout>
       <section className="container hero-intro">
         <div className="pure-u-1 pure-u-md-2-3">
-
           <p className="greeting-area">
             <span className="greeting">{pageData.site.siteMetadata.greetings[0]}</span>
             <span className="intro">, my name is</span>
@@ -41,23 +40,16 @@ export default () => {
             <p>I love to spend my free time learning more about new technologies in order to aplly on my next projects as well exploring <a target="_blank" rel="noopener noreferrer" href="https://jfreitas.photos">Canada with my camera</a></p>
             <p></p>
           </div>
-
         </div>
 
         <div className="pure-u-1 pure-u-md-1-3">
           <figure>
             <img className="headshot" src={jfreitas} alt={name.last} />
           </figure>
-
-        </div>
-
-      </section>
-
-      <section className="container">
-        <div className="pure-u-md-1-2">
-
         </div>
       </section>
+
+      <section className="unde-the-hood-area"></section>
     </Layout>
   )
 }
