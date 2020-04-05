6b6f5a8813c87453fa051dc146a0601d5492c94b
J. Freitas <juniormfreitas@gmail.com>
2020-04-05 16:31:19 -0600

    Updating content

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,45 +5,66 @@ import jfreitas from "../img/jfreitas-developer.jpg"
 import SEO from "../components/seo"
 
 export default () => {
-
   const pageData = useStaticQuery(
     graphql`
-    query Index {
+      query Index {
         site {
-            siteMetadata {
-                name {
-                  first
-                  middle
-                  last
-                }
-                greetings
-                aboutMeIntro
-                title
+          siteMetadata {
+            name {
+              first
+              middle
+              last
             }
-        }    
-    }`
+            greetings
+            aboutMeIntro
+            title
+          }
+        }
+      }
+    `
   )
 
-  const name = pageData.site.siteMetadata.name;
+  const name = pageData.site.siteMetadata.name
 
   return (
     <Layout>
       <SEO
         name={`${name.first} ${name.middle} ${name.last}`}
         title={pageData.site.siteMetadata.title}
-        aboutMeIntro={pageData.site.siteMetadata.aboutMeIntro} />
+        aboutMeIntro={pageData.site.siteMetadata.aboutMeIntro}
+      />
       <section className="container hero-intro">
         <div className="pure-u-1 pure-u-md-2-3">
           <p className="greeting-area">
-            <span className="greeting">{pageData.site.siteMetadata.greetings[0]}</span>
+            <span className="greeting">
+              {pageData.site.siteMetadata.greetings[0]}
+            </span>
             <span className="intro">, my name is</span>
           </p>
-          <h1 className="name">{name.first} {name.middle} {name.last}</h1>
+          <h1 className="name">
+            {name.first} {name.middle} {name.last}
+          </h1>
 
-          <div className="pure-u-1 pure-u-md-2-3">
-            <p className="about-area">{pageData.site.siteMetadata.aboutMeIntro}</p>
-            <p>Graduated in systems analysis, I am aficionado in helping business and individuals elevate their brands and reach the next level.</p>
-            <p>I love to spend my free time learning more about new technologies in order to aplly on my next projects as well exploring <a target="_blank" rel="noopener noreferrer" href="https://jfreitas.photos">Canada with my camera</a></p>
+          <div className="pure-u-1 pure-u-md-2-3 hero-intro-about">
+            <p>
+              I am a Full Stack Web Developer with expertise in build and
+              maintaining systems, APIs and websites. More than 6 years of
+              progressive experience in the software development industry I am
+              passionate in helping companies and individuals elevate their
+              brands and reach the next level in their business.
+            </p>
+
+            <p>
+              I love to spend my free time learning more about new technologies
+              in order to apply on my next projects as well exploring{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://jfreitas.photos"
+              >
+                Canada with my camera
+              </a>
+            </p>
           </div>
         </div>
 
