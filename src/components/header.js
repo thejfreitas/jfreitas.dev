defc0da47b232f2542a4661e3b12845de1613702
J. de Freitas <juniormfreitas@gmail.com>
2020-12-20 12:34:19 -0600

    Move main.scss import to a global file

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import "../scss/main.scss"
 import { graphql, useStaticQuery } from "gatsby"
 import MainNavigation from "../components/main-navigation"
 
