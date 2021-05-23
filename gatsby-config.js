6d53795ed1ccb36c24f25838077a65adbb0b0946
J. de Freitas <juniormfreitas@gmail.com>
2021-05-23 15:33:21 -0600

    Add more info into siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,33 +6,35 @@
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://jfreitas.dev",
-    greetings: ["Hello", "Hi", "Olá"],
+    siteUrl: 'https://jfreitas.dev',
+    siteAddress: 'jfreitas.dev',
+    greetings: ['Hello', 'Hi', 'Olá'],
     name: {
-      first: "J.",
-      middle: "de",
-      last: "Freitas",
+      first: 'J.',
+      middle: 'de',
+      last: 'Freitas',
     },
-    title: "Software Developer",
+    title: 'Software Developer',
     aboutMeIntro:
-      "I am a Full-Stack Web Developer with passion in build things with code.",
+      'I am a Full-Stack Web Developer with passion in build things with code.',
     socialMedia: [
       {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/juniormfreitas/",
-        icoClass: "linkedin",
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/juniormfreitas/',
+        icoClass: 'linkedin',
       },
       {
-        name: "Github",
-        url: "https://github.com/juniormfreitas",
-        icoClass: "github",
+        name: 'Github',
+        url: 'https://github.com/juniormfreitas',
+        icoClass: 'github',
       },
       {
-        name: "Twitter",
-        url: "https://twitter.com/juniormfreitas",
-        icoClass: "twitter",
+        name: 'Twitter',
+        url: 'https://twitter.com/juniormfreitas',
+        icoClass: 'twitter',
       },
     ],
+    repository: 'https://github.com/juniormfreitas/jfreitas.dev',
   },
   plugins: [
     {
@@ -41,33 +43,33 @@ module.exports = {
         implementation: require('sass'),
       },
     },
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-google-fonts",
+      resolve: 'gatsby-plugin-google-fonts',
       options: {
-        fonts: ["Poppins:400,500"],
-        display: "swap",
+        fonts: ['Poppins:400,500'],
+        display: 'swap',
       },
     },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "UA-75050740-3",
+        trackingId: 'UA-75050740-3',
       },
     },
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "jfreitas.dev",
-        short_name: "jfreitas.dev",
-        start_url: "/",
-        background_color: "#e34f26",
-        theme_color: "#e34f26",
-        display: "standalone",
-        icon: "src/img/favicon/icon.png",
+        name: 'jfreitas.dev',
+        short_name: 'jfreitas.dev',
+        start_url: '/',
+        background_color: '#e34f26',
+        theme_color: '#e34f26',
+        display: 'standalone',
+        icon: 'src/img/favicon/icon.png',
       },
     },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-robots-txt",
+    'gatsby-plugin-offline',
+    'gatsby-plugin-robots-txt',
   ],
 }
