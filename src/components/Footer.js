a3b553158dced0c710ad9b9ec3dd681f165d7f75
J. de Freitas <juniormfreitas@gmail.com>
2021-05-23 15:42:09 -0600

    Remove hardcoded data out of Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,34 @@
 import React from "react"
+import { graphql, useStaticQuery } from 'gatsby';
 
 const Footer = () => {
-    const year = new Date().getFullYear();
+	const year = new Date().getFullYear();
 
-    return (
-        <footer className="container pure-g">
-            <div className="pure-u-1 pure-u-md-1-2 credits">
-                <p>jfreitas.dev {year} &bull; Handcrafted by me</p>
-            </div>
-            <div className="pure-u-1 pure-u-md-1-2 code-source">
-                <p><a href="https://github.com/juniormfreitas/jfreitas.dev" target="_blank" rel="noopener noreferrer">How I built this website</a></p>
-            </div>
-        </footer>
-    )
+	const siteDataQuery = useStaticQuery(
+		graphql`
+			query FooterData {
+				site {
+					siteMetadata {
+						siteAddress
+						repository
+					}
+				}
+			}
+		`
+	);
+
+	const { siteAddress, repository } = siteDataQuery.site.siteMetadata;
+
+	return (
+		<footer className="container pure-g">
+			<div className="pure-u-1 pure-u-md-1-2 credits">
+				<p>{siteAddress} {year} &bull; Handcrafted by me</p>
+			</div>
+			<div className="pure-u-1 pure-u-md-1-2 code-source">
+				<p><a href={repository} target="_blank" rel="noopener noreferrer">How I built this website</a></p>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer;
